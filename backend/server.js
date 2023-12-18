4ab6edf2ecac8cbae7c9b55c7497bc3c756066cb
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-12-17 19:09:28 -0500

    remade repo

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send({ msg: "This is Root!" });
+});
+
+const goalsRouter = require("./routes/goals");
+app.use("/api/goals", goalsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
