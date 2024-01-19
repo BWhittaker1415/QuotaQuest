e2b2838f1a4bfb0bd62b717352741d51f17f7d02
BWhittaker1415 <bwhittaker1415@gmail.com>
2024-01-19 14:25:21 -0500

    changed port

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "../public"),
     },
-    port: 4001,
+    port: 3000,
     open: true,
     hot: true,
     compress: true,
